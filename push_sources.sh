49344346df7297accb3f9ac7ff48ded093037966
Mohanad Abugharbia <mgharbieh.01@gmail.com>
2023-07-15 21:33:43 +0200

    Made the script quiter

--- push_sources.sh
+++ push_sources.sh
@@ -8,17 +8,17 @@ STAGE=${STAGE:-"prod"}
 SOURCE_BUCKET=$(aws cloudformation describe-stack-resources --stack-name Fitness-App-Source-Bucket-${STAGE} | jq ".StackResources[].PhysicalResourceId" | sed -r 's/"//g' |grep source)
 ROOT_BUCKET=$(aws cloudformation describe-stack-resources --stack-name Fitness-App-Source-Bucket-${STAGE} | jq ".StackResources[].PhysicalResourceId" | sed -r 's/"//g' |grep root)
 
+mkdir -p lambda/dependencies/python
 cd lambda/dependencies
-mkdir python
 python3.10 -m pip install -r $WORKDIR/requirements.txt -t ./python
-zip -r python_dependencies.zip ./python
-aws s3 cp python_dependencies.zip s3://$SOURCE_BUCKET/dependencies/python_dependencies.zip
+zip -qr python_dependencies.zip ./python
+aws s3 cp --quiet python_dependencies.zip s3://$SOURCE_BUCKET/dependencies/python_dependencies.zip
 rm -r python_dependencies.zip python/
 cd $WORKDIR
 
 cd lambda/python_app
-zip -r python_app.zip .
-aws s3 cp python_app.zip s3://$SOURCE_BUCKET/functions/python_app.zip
+zip -qr python_app.zip .
+aws s3 cp --quiet python_app.zip s3://$SOURCE_BUCKET/functions/python_app.zip
 rm python_app.zip
 cd $WORKDIR
 
